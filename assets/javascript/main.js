c3f02f591ea660233ba5958b00cf6a070887d2ae
Jaime M <jaimemarsh19@gmail.com>
2023-03-09 18:40:55 -0500

    CSS circle imgs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,27 @@
 
 console.log("javascript is connected")
-
+//help from TA two different ways to make hidden and display
 // makes aboutme section appear
 function myFunction() {
     let aboutMe = document.querySelector('#aboutme');
     let displaySetting = aboutMe.style.hidden;
     // flex for flexbox
-    if (displaySetting === 'flex') {
+    if (aboutMe.style.display === 'flex') {
         aboutMe.style.display = 'none';
     } else {
         aboutMe.style.display = 'flex';
     }
-
+    console.log(displaySetting)
     console.log("Button");
+    console.log(aboutMe.style.display)
 }
 
 let btn = document.querySelector("#aboutmeBtn");
 btn.addEventListener("click", myFunction);
-
-
+//makes work section appear
 function workFunction() {
     let work = document.querySelector('#work');
-    let displaySetting = work.style.hidden;
+    let displaySetting = work.style.display;
     // flex for flexbox
     if (displaySetting === 'flex') {
         work.style.display = 'none';
