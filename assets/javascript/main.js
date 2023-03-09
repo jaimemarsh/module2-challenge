ae79d401103b5b431fea8e9264459c13dd67e7ae
Jaime M <jaimemarsh19@gmail.com>
2023-03-09 15:17:05 -0500

    forgot to push but js half working, icons added, css updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,7 +9,7 @@ function myFunction() {
     if (displaySetting === 'flex') {
         aboutMe.style.display = 'none';
     } else {
-        aboutMe.style.display = 'flex'
+        aboutMe.style.display = 'flex';
     }
 
     console.log("Button");
@@ -19,4 +19,18 @@ let btn = document.querySelector("#aboutmeBtn");
 btn.addEventListener("click", myFunction);
 
 
+function workFunction() {
+    let work = document.querySelector('#work');
+    let displaySetting = work.style.hidden;
+    // flex for flexbox
+    if (displaySetting === 'flex') {
+        work.style.display = 'none';
+    } else {
+        work.style.display = 'flex'
+    }
+
+    console.log("Button");
+}
+let workBtn = document.querySelector("#workBtn");
+workBtn.addEventListener("click", workFunction);
 
