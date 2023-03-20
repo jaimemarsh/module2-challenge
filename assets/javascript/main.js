7b1ae555f72ea2463c91bf4cf39b82542340a821
Jaime M <jaimemarsh19@gmail.com>
2023-03-20 00:06:08 -0400

    added sticky nav bar, hamburger menu, and more mobile design

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,24 @@
 
 console.log("javascript is connected")
-//help from TA two different ways to make hidden and display
-// makes aboutme section appear
-function myFunction() {
+
+// makes aboutme section appear and dissapear
+function navFunction() {
+    let nav = document.querySelector('#nav');
+    let displaySetting = nav.style.display;
+    // flex for flexbox
+    if (displaySetting === 'flex') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'flex';
+    }
+    console.log("Nav Button")
+}
+
+let navBtn = document.querySelector("#hamburgerBtn");
+navBtn.addEventListener("click", navFunction);
+
+// makes aboutme section appear and dissapear
+function aboutmeFunction() {
     let aboutMe = document.querySelector('#aboutme');
     let displaySetting = aboutMe.style.hidden;
     // flex for flexbox
@@ -11,14 +27,14 @@ function myFunction() {
     } else {
         aboutMe.style.display = 'flex';
     }
-    console.log(displaySetting)
-    console.log("Button");
-    console.log(aboutMe.style.display)
+    console.log("Aboutme Button");
 }
 
 let btn = document.querySelector("#aboutmeBtn");
-btn.addEventListener("click", myFunction);
-//makes work section appear
+btn.addEventListener("click", aboutmeFunction);
+
+
+//makes work section appear and dissapear
 function workFunction() {
     let work = document.querySelector('#work');
     let displaySetting = work.style.display;
@@ -29,9 +45,27 @@ function workFunction() {
         work.style.display = 'flex'
     }
 
-    console.log("Button");
+    console.log("Work Button");
 }
 let workBtn = document.querySelector("#workBtn");
 workBtn.addEventListener("click", workFunction);
 
-//lol
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { scrollFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("navScroll");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scrollFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+    console.log("scroll")
+}
